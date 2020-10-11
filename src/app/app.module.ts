5d96ef758baa81f286742acdcf12ed52c37bf123
Flint64 <twda96@gmail.com>
2020-10-11 17:54:09 -0600

    upload everything?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+
+// import { Instafeed } from '../../node_modules/angular-instafeed';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    // Instafeed
   ],
   providers: [],
   bootstrap: [AppComponent]
